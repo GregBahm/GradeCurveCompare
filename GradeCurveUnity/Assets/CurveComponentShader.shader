b88955ad0c7a95bf361dd1967683155e3b717409
derp derp <workingbahm@yahoo.com.sg>
2018-06-27 02:02:02 -0700

    Initial commit

---
+++ GradeCurveUnity/Assets/CurveComponentShader.shader
@@ -0,0 +1,80 @@
+﻿Shader "Unlit/CurveComponentShader"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float3 objSpace : TEXCOORD1;
+				float3 modifiedObjSpace : TEXCOORD2;
+				float4 vertex : SV_POSITION;
+			};
+
+			float _A;
+			float _B;
+			float _C;
+			float _D;
+			float _E;
+
+			float GetCurvePoint(float param)
+			{
+				float ab = lerp(_A, _B, param);
+				float bc = lerp(_B, _C, param);
+				float cd = lerp(_C, _D, param);
+				float de = lerp(_D, _E, param);
+
+				float abc = lerp(ab, bc, param);
+				float bcd = lerp(bc, cd, param);
+				float cde = lerp(cd, de, param);
+
+				float abcd = lerp(abc, bcd, param);
+				float bcde = lerp(bcd, cde, param);
+
+				return lerp(abcd, bcde, param);
+			}
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.objSpace = v.vertex + .5;
+
+				float heightStartLerp = saturate(o.objSpace.y * 2);
+				float heightEndLerp = saturate(o.objSpace.y * 2 - 1);
+				float height = GetCurvePoint(o.objSpace.y);
+				v.vertex.z = (o.objSpace.z) * height;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.modifiedObjSpace = v.vertex + .5;
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float3 col = lerp(float3(0, 1, 0), float3(1, 0, 0), i.objSpace.y);
+				col *= i.modifiedObjSpace.z / 20;
+				return float4(col, 1);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ GradeCurveUnity/Assets/MainScript.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public TextAsset DataSource;
+    public GameObject GraphPart;
+    public float RowWidth;
+    public float RowMargin;
+
+    private List<SubjectBehaviour> _behaviours;
+
+    void Start()
+    {
+        string[] entries = DataSource.text.Split('\n');
+        List<SubjectData> data = entries.Select(ParseDataSource).ToList();
+        data = data.OrderBy(item => item.AStar).ToList();
+        _behaviours = new List<SubjectBehaviour>();
+        foreach (SubjectData datum in data)
+        {
+            GameObject obj = new GameObject(datum.SubjectName);
+            SubjectBehaviour behaviour = obj.AddComponent<SubjectBehaviour>();
+            behaviour.Mothership = this;
+            behaviour.Data = datum;
+            obj.transform.parent = transform;
+            _behaviours.Add(behaviour);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _behaviours.Count; i++)
+        {
+            float pos = i * (RowWidth + RowMargin);
+            _behaviours[i].transform.position = new Vector3(pos, 0, 0);
+            _behaviours[i].transform.localScale = new Vector3(RowWidth, 1, 1);
+        }
+    }
+
+    private SubjectData ParseDataSource(string line)
+    {
+        string[] components = line.Split('\t');
+        SubjectData ret = new SubjectData();
+        ret.SubjectName = components[0];
+        ret.AStar = Convert.ToSingle(components[1]);
+        ret.A = Convert.ToSingle(components[2]);
+        ret.B = Convert.ToSingle(components[3]);
+        ret.C = Convert.ToSingle(components[4]);
+        ret.D = Convert.ToSingle(components[5]);
+        ret.E = Convert.ToSingle(components[6]);
+        ret.Other = Convert.ToSingle(components[7]);
+        ret.Students = Convert.ToInt32(components[8]);
+        return ret;
+    }
+}
+
+public class SubjectBehaviour : MonoBehaviour
+{
+    public MainScript Mothership;
+    public SubjectData Data;
+    public Material mat;
+    
+    private void Start()
+    {
+        GameObject bar = Instantiate(Mothership.GraphPart);
+        bar.name = name;
+        bar.transform.parent = this.transform;
+        MeshRenderer renderer = bar.GetComponent<MeshRenderer>();
+        mat = renderer.material;
+    }
+
+    private void Update()
+    {
+        mat.SetFloat("_A", Data.AStar);
+        mat.SetFloat("_B", Data.A);
+        mat.SetFloat("_C", Data.B);
+        mat.SetFloat("_D", Data.C);
+        mat.SetFloat("_E", Data.D);
+        mat.SetFloat("_F", Data.E);
+    }
+}
+
+public class SubjectData
+{
+    public string SubjectName;
+    public float AStar;
+    public float A;
+    public float B;
+    public float C;
+    public float D;
+    public float E;
+    public float Other;
+    public int Students;
+}
